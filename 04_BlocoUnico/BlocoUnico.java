e055089168fc3ca7f1e66e3151ef53e85e6b6062
gustavo <editorkuchi@gmail.com>
2024-03-15 11:13:38 -0300

    metodo 3

--- 04_BlocoUnico/BlocoUnico.java
+++ 04_BlocoUnico/BlocoUnico.java
@@ -10,23 +10,26 @@ public class BlocoUnico{
         public static void main (String args[]){
         Scanner entrada = new Scanner(System.in);
         
-        double nro1, nro2;
+        double nro1, nro2, nro3;
         separador();
         System.out.println ("Informe o número 1:");
         nro1 = entrada.nextDouble();
         separador();
         System.out.println ("Informe o número 2:");
         nro2 = entrada.nextDouble();
-               
         separador();
-        System.out.println ("A média calculada foi: " + media(nro1, nro2));
+        System.out.println ("Informe o número 3:");
+        nro3 = entrada.nextDouble();    
+
+        separador();
+        System.out.println ("A média calculada foi: " + media(nro1, nro2, nro3));
     }
     public static void separador() {
     System.out.println ("********************");
 }
 
-    public static double media(double nro1, double nro2) {
-        return (nro1 + nro2) /2;
+    public static double media(double nro1, double nro2, double nro3) {
+        return (nro1 + nro2 + nro3) /3;
     }
 }
 
