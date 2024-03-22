9fa597a42ffb00ee0d6279a8cb260886b9fcef4a
gustavo <editorkuchi@gmail.com>
2024-03-22 14:24:06 -0300

    COISARADA

--- Biblioteca/BibliotecaTeste.java
+++ Biblioteca/BibliotecaTeste.java
@@ -7,65 +7,74 @@
  * @version (V 1.0)
  */
 
-public class Biblioteca { // classe  biblioteca que sera utilizada para definir os metodos de formulas.
-    public static double areaCirculo (double raio){
+import java.util.Scanner;
+
+public class BibliotecaTeste { // classe  biblioteca que sera utilizada para definir os metodos de formulas.
+    
+    public static void areaCirculo (double raio){
+        Scanner input = new Scanner(System.in);
         double area;
+        
+        System.out.println("=== CALCULE A AREA DO CIRCULO ===");
+        System.out.println("Valor do raio: ");
+        raio = input.nextDouble();
+        
         area = Math.PI * (raio);
-        return area;
+        System.out.println("Area do circulo: " + area);
     };
-    public static double areaRetangulo (double base, double altura){
+    public double areaRetangulo (double base, double altura){
         double area;
         area = base * altura;
         return area;
     };
-    public static double areaQuadrado (double lado){
+    public double areaQuadrado (double lado){
         double area;
         area = lado * lado;
         return area;
     };
-    public static double areaTriangulo (double base, double altura){
+    public double areaTriangulo (double base, double altura){
         double area;
         area = (base * altura)/2;
         return area;
     };
-    public static double perimetroCirculo (double raio){
+    public double perimetroCirculo (double raio){
         double perimetro;
         perimetro = 2 * Math.PI * raio;
         return perimetro;
     };
-    public static double perimetroRetangulo (double base, double altura){
+    public double perimetroRetangulo (double base, double altura){
         double perimetro;
         perimetro = 2 * (base + altura);
         return perimetro;
     };
-    public static double perimetroQuadrado (double lado){
+    public double perimetroQuadrado (double lado){
         double perimetro;
         perimetro = 4 * lado;
         return perimetro;
     };
-    public static double perimetroTriangulo (double lado){
+    public double perimetroTriangulo (double lado){
         double perimetro;
         perimetro = 3 * lado;
         return perimetro;
     };
-    public static double volumeEsfera (double raio){
+    public double volumeEsfera (double raio){
         double volume;
         volume = (4.0/3) * Math.PI * (raio * raio * raio); // lembrar sempre do .0 apos o numero dentro de parenteses para identificar como double
         return volume;
     };  
-    public static double volumeCilindro (double raio, double altura){
+    public double volumeCilindro (double raio, double altura){
         double volume;
         volume = Math.PI * (raio * raio) * altura;
         return volume;
     };
-    public static double volumeCubo (double lado){
+    public double volumeCubo (double lado){
         double volume;
         volume = lado * lado * lado;
         return volume;
     };
     
     // metodo booleano com funçao condicional para identificar se o X sera par ou impar
-    public static boolean numParImpar (int x){
+    public boolean numParImpar (int x){
        
         if (x % 2 == 0){
             return true;
@@ -75,7 +84,7 @@ public class Biblioteca { // classe  biblioteca que sera utilizada para definir 
     }
     
     // metodo booleano com funçao condicional para identificar se o inteiro X eh multiplo do inteiro Y
-    public static boolean numMultiplo (int x, int y){
+    public boolean numMultiplo (int x, int y){
         if (x % y == 0){
             return true;
         } else {
@@ -84,7 +93,7 @@ public class Biblioteca { // classe  biblioteca que sera utilizada para definir 
     }
     
     // metodo com funçao condicional para identificar o MAIOR dentre os 3 valores declarados
-    public static double numMaiorValor (double x, double y, double z){
+    public double numMaiorValor (double x, double y, double z){
         if (x >= y && x >= z){
             return x;
         } else if (y >= x && y >= z){
@@ -93,7 +102,7 @@ public class Biblioteca { // classe  biblioteca que sera utilizada para definir 
     }
     
     // metodo com funçao condicional para identificar o MENOR dentre os 3 valores declarados
-    public static double numMenorValor (double x, double y, double z){
+    public double numMenorValor (double x, double y, double z){
         if (x <= y && x <= z){
             return x;
         } else if (y <= x && y <= z){
@@ -102,7 +111,7 @@ public class Biblioteca { // classe  biblioteca que sera utilizada para definir 
     }
     
     // metodo que identifica o valor INTERMEDIARIO dentre os 3 valores declarados, somando os tres valores e subtraindo o maior e o menor valor;
-    public static double numValorIntermediario (double x, double y, double z){
+    public double numValorIntermediario (double x, double y, double z){
         double intermediario1, intermediario2, maior, menor;
         
         maior = numMaiorValor(x, y, z);
--- Biblioteca/TesteMetodos.java
+++ Biblioteca/TesteMetodos.java
@@ -1,26 +1,18 @@
 
 /**
- * Escreva uma descrição da classe CalcMedia aqui.
+ *  TesteMetodos, como o nome ja diz, se trata da classe responsavel por realizar o teste de todos os metodos declarados na
+ *  biblioteca nomeada criativamente como Biblioteca!
  * 
- * @author (seu nome) 
- * @version (um número da versão ou uma data)
+ * @author (Gustavo Flores) 
+ * @version (22.03.2024)
  */
 
 import java.util.Scanner;
+
 public class TesteMetodos {
-    public static void main (){
-        Scanner teclado = new Scanner(System.in);
-        
-        System.out.println("Insira o primeiro valor: ");
-        double x = teclado.nextDouble();
-        System.out.println("Insira o segundo valor: ");
-        double y = teclado.nextDouble();
-        System.out.println("Insira o terceiro valor: ");
-        double z = teclado.nextDouble();
-        
-        double retorno = Biblioteca.maiorValor(x, y, z);
-        
-        System.out.println(retorno);
+    public static void main (String args[]){
+        // FODASEEEEEEEEEEEEEEEEEEEEEEE PORRAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA43T00U034TJ03JT4WJT3J0J9TJ9J0T3J90TJ9J93     VAMO DO INICIO ENTAO JA Q TA DIFICIL MERDA BOSTA
     }
 }
+
 
