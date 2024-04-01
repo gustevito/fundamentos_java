484aebaa05cc5fc5424504c59f3b08a9c47b3619
gustavo <editorkuchi@gmail.com>
2024-04-01 11:09:47 -0300

    forLoop

--- Salario/SalarioFuncionarios.java
+++ Salario/SalarioFuncionarios.java
@@ -7,16 +7,22 @@ public class SalarioFuncionarios{
         double horasValor;
         double salario;
         
-        System.out.println("Informe o tempo em HORAS trabalhadas: ");
-        horas = teclado.nextDouble();
-        
-        System.out.println("Informe o VALOR (R$) por hora apenas em numeros: ");
-        horasValor = teclado.nextDouble();
-        
-        salario = BibliotecaSalario.calcSalario(horas, horasValor);
-        System.out.println("Seu salário é de R$" + salario);
+            for (int cont = 1; cont < 6 ; cont++){ // for (<valor inicial da varivavel>; <valor limite da variavel>; <funçao que o for vai executar>)
+               
+                System.out.println("FUNCIONARIO" + cont);
+            
+                System.out.println("Informe o tempo em HORAS trabalhadas: ");
+                horas = teclado.nextDouble();
+                
+                System.out.println("Informe o VALOR (R$) por hora apenas em numeros: ");
+                horasValor = teclado.nextDouble();
+                
+                salario = BibliotecaSalario.calcSalario(horas, horasValor);
+                System.out.println("Seu salário é de R$" + salario);
+            
+        }
     }
-    
+
     
     // se eu quiser definir o método de calculo dentro da própria classe, é só eu fazer o seguinte:
     
